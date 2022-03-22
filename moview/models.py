69db282f634ce7d1035a7f248f45647e2568f5e3
Jyuri <jeon1310@gmail.com>
2022-03-22 14:20:08 +0900

    model changes

--- moview/models.py
+++ moview/models.py
@@ -7,15 +7,15 @@ import time
 
 # Create your models here.
 class Moviews(models.Model):
-    datetime = models.DateTimeField(primary_key=True,default=datetime.fromtimestamp(time.time()))
+    id = models.AutoField(primary_key=True)
     moviename = models.CharField(max_length=20)
     category = models.CharField(max_length=10)
-    moviewline = models.CharField(max_length=50)
+    moviewline = models.CharField(max_length=55)
     viewdate = models.DateField()
     story = models.TextField()
     moviewimg = models.ImageField(upload_to="moview/", blank=True,null=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)  # 장고 유저 가져오기
 
     def __str__(self):
-        return self.moviename
+        return self.moviewline
 
