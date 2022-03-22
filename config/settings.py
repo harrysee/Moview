adcb023491a41d5b7330eec46eb7214b9250eafd
Jyuri <jeon1310@gmail.com>
2022-03-22 19:47:16 +0900

    static copyed

--- config/settings.py
+++ config/settings.py
@@ -133,7 +133,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [  # 디렉터리 추가
     BASE_DIR / 'static',
